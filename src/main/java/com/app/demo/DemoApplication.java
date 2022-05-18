453d0cbc367a81cd470917a7ead0f8c187d40906
jeffreyzheng <794241572@qq.com>
2022-05-18 17:31:38 +0800

    test(case): 新建ci_test分支

---
+++ src/main/java/com/app/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tianfei
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
